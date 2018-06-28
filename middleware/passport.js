eed0d29c44db3fcf15e96e8916beec878e250344
brian schardt <Brian@apples-MacBook-Pro.local>
2018-06-28 16:17:40 -0500

    standarized

--- middleware/passport.js
+++ middleware/passport.js
@@ -1,5 +1,5 @@
-const JwtStrategy   = require('passport-jwt').Strategy;
-const ExtractJwt    = require('passport-jwt').ExtractJwt;
+const { JwtStrategy}= require('passport-jwt');
+const { ExtractJwt} = require('passport-jwt');
 const { User }      = require('../models');
 const CONFIG        = require('../config/config');
 const {to}          = require('../services/util.service');
